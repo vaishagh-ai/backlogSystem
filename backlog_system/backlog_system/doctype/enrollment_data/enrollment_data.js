3b1424d6937cd07c02e5aa0af9a8bc507adb6a61
Avanthika <avanthikak126@gmail.com>
2026-03-03 00:25:39 +0530

    Cleanup repo: ignore logs, env, sites, project folders

--- backlog_system/backlog_system/doctype/enrollment_data/enrollment_data.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Administrator and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Enrollment Data", {
-// 	refresh(frm) {
-
-// 	},
-// });
